7c25abf14d97f15e13ee5808c35de728949ae7ad
Areebah Iqbal <g00078814@aus.edu>
2021-01-03 14:37:15 +0400

    update

--- src/com/areebahsci/gui_project/main/Main.java
+++ src/com/areebahsci/gui_project/main/Main.java
@@ -1,12 +1,12 @@
 package com.areebahsci.gui_project.main;
 
-import com.areebahsci.gui_project.controller.StudentMenuController;
+import com.areebahsci.gui_project.controller.Controller;
 
 public class Main { 
 
 	public static void main(String[] args) {
 		
 		// just runs an instance of the controller which handles the model and view 
-		new StudentMenuController();
+		new Controller();
 	}
 }
